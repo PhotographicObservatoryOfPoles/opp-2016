4ecaca9f22005b603aa18685743a30d5830a4200
Audrey Guénée <audreyguenee@free.fr>
2017-01-26 22:56:06 +0100

    [CONFIG] add cron tasks for cleaning backups

---
+++ config/cron/task.clean-uploads-backups.php
@@ -0,0 +1,27 @@
+<?php
+
+$directory = '${wp.path.backupfolder}/';
+$files = scandir($directory);
+
+if ($files) {
+
+	// Remove . and .. values from array
+	unset($files[0]);
+	unset($files[1]);
+
+	// Sort files by filemtime desc
+	$success = usort($files, function($file1, $file2) {
+		$dir = '${wp.path.backupfolder}/';
+    	return filemtime($dir . $file1) > filemtime($dir . $file2);
+	});
+
+	if ($success) {
+
+		// Remove older files to keep the 4 most recent files
+		$nbFilesToDelete = count($files) - 4;
+		
+		for ($i=0; $i < $nbFilesToDelete; $i++) {
+			exec('rm ' . $directory . $files[$i]);
+		}
+	}
+}
